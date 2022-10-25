baf83027fa58ef8d96d3b9050ebaa90bc093d62c
babe-saleh-mahfoud <babesalehmahfoud@gmail.com>
2022-10-25 22:11:34 +0100

    fixed bug

--- encrypt_file.py
+++ encrypt_file.py
@@ -3,15 +3,13 @@ from cryptography.fernet import Fernet
 
 class EncryptFile:
     def encrypt(self, files):
-
+        encryptionKey = Fernet.generate_key()
+        keyHandler = open("key.txt", "wb")
+        keyHandler.write(encryptionKey)
         for file in files:
-            print()
             writeHandler = open((file), "wb")
             readHandler = open((file), "rb")
             file = readHandler.read()
-            encryptionKey = Fernet.generate_key()
-            keyHandler = open("key.txt", "wb")
-            keyHandler.write(encryptionKey)
             print("Wrote encryption key to 'key.txt' file")
             fernet = Fernet(encryptionKey)
             encrypted = fernet.encrypt(file)
